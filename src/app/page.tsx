671b7f053842323361ae56893e36a29d96a9a06c
rozycos <rozycos@gmail.com>
2026-05-02 22:03:16 +0200

    text edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,25 @@ export default async function BlogPage() {
         </p>
       </header>
 
+      <div className="mb-16">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+          Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. 
+          Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. 
+          At etiam Athenis, ut e.
+          Sis aequitate, quam ostendis. Sed uti oratione perpetua malo quam interrogare aut interrogari. 
+          Ut placet, inquam. Tum dicere exorsus est. 
+          Primum igitur, inquit, sic agam, ut ipsi auctori huius disciplinae placet: constituam, quid et quale sit id, de quo quaerimus, non quo ignorare vos arbitrer, sed ut ratione et via procedat oratio. 
+          Quaerimus igitur, quid sit extremum et ultimum bonorum, quod omnium philosophorum sententia.
+          Familiari suo, geometrica discere maluisset quam illum etiam ipsum dedocere. 
+          Sol Democrito magnus videtur, quippe homini erudito in geometriaque perfecto, huic pedalis fortasse; 
+          tantum enim esse censet, quantus videtur, vel paulo aut maiorem aut minorem. 
+          Ita, quae mutat, ea corrumpit, quae sequitur sunt tota Democriti, atomi, inane, imagines, quae eidola nominant, quorum incursione non solum.
+        </p>
+      </div>
+
       <div className="grid grid-cols-1 gap-12">
+        <h2>POSTY</h2>
         {posts.map((post) => (
           <article key={post.id} className="group flex flex-col md:flex-row gap-8 items-start">
             {/* Miniaturka */}
